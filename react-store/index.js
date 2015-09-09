b613747b5adab5db6f1524a8fc8df183ee302204
haoxin <coderhaoxin@outlook.com>
2015-09-09 11:43:21 +0800

    remove react-store, see `https://github.com/onebook/mini-react-store`

--- react-store/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import { Item1, Item2 } from './component/item'
-import React from 'react'
-
-window.init = function() {
-  React.render(<Item1 />, document.querySelector('#item1'))
-  React.render(<Item2 />, document.querySelector('#item2'))
-}
